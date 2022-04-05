fcb6f818389fa958c64c0476b333d57c7c3c00e7
murongqimiao <murongqimiao@live.cn>
2022-04-05 17:07:56 +0800

    feat: update

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -3,29 +3,29 @@ module.exports = {
     description: 'Just playing around',
     themeConfig: {
       nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Guide', link: '/guide/' },
+        { text: 'Home', link: '/joaoBlog/' },
+        { text: 'Guide', link: '/' },
       ],
       // sidebar: ['/', '/game/', '/life/']
       sidebar: [
         {
           title: '首页',   // 必要的
-          path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          path: '/joaoBlog/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
           collapsable: false, // 可选的, 默认值是 true,
           sidebarDepth: 1,    // 可选的, 默认值是 1
         },
         {
           title: 'DAG-Diagram有向无环图',
-          path: '/dag/',
+          path: '/joaoBlog/dag',
           children: [
-            '/dag/', '/dag/0'
+            '/joaoBlog/dag/', '/joaoBlog/dag/0'
           ]
         },
         {
           title: '交互帧动画游戏',
-          path: '/game/',
+          path: '/joaoBlog/game',
           children: [
-            '/game/', '/game/0'
+            '/joaoBlog/game/', '/joaoBlog/game/0'
           ],
           initialOpenGroupIndex: -1 // 可选的, 默认值是 0
         }
